22832ef1ca8d47d2701dd8a321a4ae3007f622a2
Anthony <azaniolo@hawk.iit.edu>
2018-11-12 21:23:49 -0600

    Add function to check if browser has JavaScript and add a button to index.html

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,23 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+"use strict";
+(function() {
+  if (typeof(document.querySelector)==='undefined') {
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var doorbell_button;
+    document.querySelector('html').className='js';
+
+    doorbell_button = document.createElement('button');
+    doorbell_button.id = 'doorbell-button';
+    doorbell_button.innerHTML = 'Ring the Doorbell';
+
+    doorbell_button.addEventListener('click', function(){
+      var doorbell = new Audio('media/doorbell.mp3');
+      doorbell.play();
+    });
+
+    document.querySelector('#content').appendChild(doorbell_button);
+  });
+
+})();
