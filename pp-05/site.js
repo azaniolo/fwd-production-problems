b676bc0d16b8cbe5290e9270faad8dbc2617f0c7
Anthony <azaniolo@hawk.iit.edu>
2018-11-19 21:59:34 -0600

    Complete pp-05

--- pp-05/site.js
+++ pp-05/site.js
@@ -5,7 +5,7 @@
 var x = 5;
 
 function double(num) {
-  x = num * 2;
+  var x = num * 2;
   return x;
 }
 
@@ -18,23 +18,48 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  function can be accessed via the `window` global object,
 //  like `window.x`):
 
+(function() {
+  var x = 5;
+
+  function double(num) {
+    var x = num * 2;
+    return x;
+  }
+
+  double(6);
+  console.log('The value of x is:', x, 'It should be 5.');
+})();
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:
 
 function arrayEach(array, func) {
-  for (i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     func(array[i]);
   }
 }
 
 arrayEach(['red','green','blue'], console.log);
 
-console.log(i) // should be 'undefined', not 3
+// For the sake of not adding another global variable to get the desired
+// undefined result.
+
+try {
+  console.log(i); // should be 'undefined', not 3
+}
+catch {
+  console.log("Variable 'i' is undefined.");
+}
 
 //  4. Explain why this function does not modify the global
 //  variable x declared on line 5 above. Write your explanation
 //  as JavaScript comments.
+
+/* Since the variable x is also the name of the argument, it is treated as a
+local variable and takes precedence over the global variable.  If the argument
+name within the function was anything else, the global value of x would be
+changed.  The same result could be achieved with a different variable name
+by putting var before the x therefore creating a locally scoped variable x. */
 
 function addTwo(x) {
   x = x + 2;
